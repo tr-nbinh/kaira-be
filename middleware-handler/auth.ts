6d38f1bd806e7f92dd767d047e41e13fa45c8b94
tr-nbinh <nhatbinh211120@gmail.com>
2025-07-24 19:14:55 +0700

    use regex instead of string for public apis

--- middleware-handler/auth.ts
+++ middleware-handler/auth.ts
@@ -7,14 +7,24 @@ export interface AuthenticatedRequest extends NextRequest {
 	user?: { id: number; [key: string]: any } | null; // Mở rộng thêm các thuộc tính user nếu cần
 }
 
-const PUBLIC_API_PATHS = ["/api/banners", "/api/categories", "/api/products", "/api/testimonials", "/api/menus", '/api/colors', '/api/sizes', '/api/brands'];
+const PUBLIC_API_PATTERNS = [
+	/^\/api\/banners$/,
+	/^\/api\/categories$/,
+	/^\/api\/products$/, // Khớp chính xác /api/products
+	/^\/api\/products\/[^\/]+$/, // Khớp /api/products/BẤT_KỲ_GÌ (ví dụ: /api/products/16, /api/products/abc)
+	/^\/api\/testimonials$/,
+	/^\/api\/menus$/,
+	/^\/api\/colors$/,
+	/^\/api\/sizes$/,
+	/^\/api\/brands$/,
+];
 const PUBLIC_API_PREFIX = "/api/auth";
 
 export async function authMiddleware(req: AuthenticatedRequest): Promise<NextResponse | null> {
 	const { t } = await getApiI18nContext(req);
 
 	const pathname = req.nextUrl.pathname;
-	const isPublicApiPath = PUBLIC_API_PATHS.includes(pathname) || pathname.startsWith(PUBLIC_API_PREFIX);
+    const isPublicApiPath = PUBLIC_API_PATTERNS.some(pattern => pattern.test(pathname)) || pathname.startsWith(PUBLIC_API_PREFIX);
 	if (isPublicApiPath) {
 		return null; // Trả về null để chỉ ra rằng không có lỗi và request có thể tiếp tục
 	}
