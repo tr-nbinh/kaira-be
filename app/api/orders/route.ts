e4c401030e23cc92b597004846d679c70cc7eb19
tr_nbinh <nhatbinh211120@gmail.com>
2026-03-25 23:21:27 +0700

    error order api fixes

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -6,73 +6,64 @@ import { handleApiError } from "@/lib/utils/handleError";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-	try {
-		const { t } = await getApiI18nContext(req);
-
-		const userId = getAuthenticatedUserId(req);
-		if (!userId) {
-			return response({ message: t("auth.unauthorized") }, 401);
-		}
-
-		const { orderItems, ...data } = await req.json();
-		if (!orderItems || !orderItems.length) {
-			return response({ message: t("order.invalid") }, 400);
-		}
-
-		const subTotal = orderItems.reduce((acc: any, curr: any) => acc + curr.finalPrice * curr.quantity, 0);
-
-		const result = await db.$transaction(async (tx) => {
-			const newOrder = await tx.order.create({
-				data: {
-					addressId: data.addressId,
-					shippingFee: 20,
-					totalAmount: subTotal + 20,
-					paymentMethod: data.paymentMethod,
-					note: data.note,
-					userId: userId,
-				},
-			});
-			if (!newOrder) {
-				return response({ message: t("order.failed") }, 400);
-			}
-
-			const orderItemsData = await orderItems.map((item: any) => ({
-				orderId: newOrder.id,
-				variantId: item.variantId,
-				quantity: item.quantity,
-				price: item.finalPrice,
-			}));
-			await tx.orderItem.createMany({
-				data: orderItemsData,
-			});
-
-			const cart = await db.cart.findUnique({
-				where: { userId: userId },
-			});
-			if (!cart) {
-				return response({ message: t("cart.cart_not_found") }, 400);
-			}
-
-			const updateStock = Promise.all(
-				orderItems.map((item: any) => {
-					return tx.productVariant.update({
-						where: { id: item.variantId },
-						data: { quantityInStock: { decrement: item.quantity } },
-					});
-				})
-			);
-			const deleteFromCart = tx.cartItem.deleteMany({
-				where: {
-					cartId: cart.id,
-				},
-			});
-			await Promise.all([updateStock, deleteFromCart]);
-
-			return newOrder;
-		});
-
-		return response({ data: result, message: "" }, 201);
-	} catch (error) {
-		return handleApiError(error);
-	}
+	// try {
+	// 	const { t } = await getApiI18nContext(req);
+	// 	const userId = getAuthenticatedUserId(req);
+	// 	if (!userId) {
+	// 		return response({ message: t("auth.unauthorized") }, 401);
+	// 	}
+	// 	const { orderItems, ...data } = await req.json();
+	// 	if (!orderItems || !orderItems.length) {
+	// 		return response({ message: t("order.invalid") }, 400);
+	// 	}
+	// 	const subTotal = orderItems.reduce((acc: any, curr: any) => acc + curr.finalPrice * curr.quantity, 0);
+	// 	const result = await db.$transaction(async (tx) => {
+	// 		const newOrder = await tx.order.create({
+	// 			data: {
+	// 				addressId: data.addressId,
+	// 				shippingFee: 20,
+	// 				totalAmount: subTotal + 20,
+	// 				paymentMethod: data.paymentMethod,
+	// 				note: data.note,
+	// 				userId: userId,
+	// 			},
+	// 		});
+	// 		if (!newOrder) {
+	// 			return response({ message: t("order.failed") }, 400);
+	// 		}
+	// 		const orderItemsData = await orderItems.map((item: any) => ({
+	// 			orderId: newOrder.id,
+	// 			variantId: item.variantId,
+	// 			quantity: item.quantity,
+	// 			price: item.finalPrice,
+	// 		}));
+	// 		await tx.orderItem.createMany({
+	// 			data: orderItemsData,
+	// 		});
+	// 		const cart = await db.cart.findUnique({
+	// 			where: { userId: userId },
+	// 		});
+	// 		if (!cart) {
+	// 			return response({ message: t("cart.cart_not_found") }, 400);
+	// 		}
+	// 		const updateStock = Promise.all(
+	// 			orderItems.map((item: any) => {
+	// 				return tx.productVariant.update({
+	// 					where: { id: item.variantId },
+	// 					data: { quantityInStock: { decrement: item.quantity } },
+	// 				});
+	// 			})
+	// 		);
+	// 		const deleteFromCart = tx.cartItem.deleteMany({
+	// 			where: {
+	// 				cartId: cart.id,
+	// 			},
+	// 		});
+	// 		await Promise.all([updateStock, deleteFromCart]);
+	// 		return newOrder;
+	// 	});
+	// 	return response({ data: result, message: "" }, 201);
+	// } catch (error) {
+	// 	return handleApiError(error);
+	// }
 }
